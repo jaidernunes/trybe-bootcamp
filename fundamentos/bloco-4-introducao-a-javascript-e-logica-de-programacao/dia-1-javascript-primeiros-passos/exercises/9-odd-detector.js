fd94152dd2740135c4a637a7d860001eceebe2fd
jaidernunes <jaidernf@gmail.com>
2022-07-21 21:31:22 -0300

    4.1 exercises 8, 9 , 10, 11 :ok:

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercises/9-odd-detector.js
@@ -0,0 +1,11 @@
+const num1 = Math.floor(Math.random() * 101);
+const num2 = Math.floor(Math.random() * 101);
+const num3 = Math.floor(Math.random() * 101);
+
+console.log(num1, num2, num3)
+
+if ((num1 % 2 !== 0) || (num2 % 2 !== 0) || (num3 % 2 !== 0)) {
+    console.log("true")
+} else {
+    console.log ("false")
+}
