3e02e22657c4aa96e8c80fcaf2f80d1c824789e6
jaidernunes <jaidernf@gmail.com>
2022-07-23 14:07:36 -0300

    updating branch

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercises/1-log.js
@@ -0,0 +1,7 @@
+// In this first exercise, go through the array printing all values contained therein with the console.log () function;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let number of numbers) {
+    console.log(number)
+}
