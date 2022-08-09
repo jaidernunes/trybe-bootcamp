ece6d0d9feaab96fab04488d087527e9b9b4069c
jaidernunes <jaidernf@gmail.com>
2022-08-09 19:40:53 -0300

    6.4 git drama

--- fundamentos/block-5-javascript-dom-events-and-web-storage/day-3-javascript-events/exercises/script.js
+++ fundamentos/block-5-javascript-dom-events-and-web-storage/day-3-javascript-events/exercises/script.js
@@ -14,8 +14,7 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
-
-//   🚀 Exercício 1:
+//   🚀 Exercício 1:  
 //   Crie um calendário dinamicamente.
 //   O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
 //   Note que os dias 29 e 30 de novembro estão no array, pois representam respectivamente Domingo e Segunda-feira.
@@ -26,6 +25,39 @@ function createDaysOfTheWeek() {
 //   Copiar
 //   let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDaysOfTheMonth() {
+  let getDaysList = document.querySelector('#days');
+
+  for (let index = 0; index < decemberDaysList.length; index += 1) {
+    
+    let day = decemberDaysList[index];
+    let dayItem = document.createElement('li');
+    dayItem.innerHTML = day;
+    
+    if (day === 24 || day === 31) {
+      // Caso o dia for 24 ou 31
+      dayItem.className = 'day holiday'; // Atribua a classe 'day holiday' ao li criado
+      getDaysList.appendChild(dayItem); // Anexe o li criado como elemento filho do ul
+    } else if (day === 4 || day === 11 || day === 18) {
+      // Caso o dia for 4, 11 ou 18
+      dayItem.className = 'day friday'; // Atribua a classe 'day friday' ao li criado
+      getDaysList.appendChild(dayItem);
+    } else if (day === 25) {
+      // Caso o dia for 25
+      dayItem.className = 'day holiday friday'; // Atribua a classe 'day holiday friday' ao li criado
+      getDaysList.appendChild(dayItem);
+    } else {
+      // Caso seja qualquer outro dia adicione a classe day ao li criado
+      dayItem.className = 'day';
+      getDaysList.appendChild(dayItem);
+    }
+  }
+}
+
+  createDaysOfTheMonth();
+  
 
 
 //   🚀 Exercício 2:
